1858f81615797b79482185c21e34cd51ca58c6ed
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-05 14:52:04 +0100

    update Test Class and Utils Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,28 @@ package org.example;
 
 //import org.example.Utils.UtilsLibrary;
 
+import org.example.Utils.PasswordValidator;
 import org.example.Utils.UtilsLibrary;
+
+import java.util.Scanner;
 
 public class Main {
     static void main() {
-        UtilsLibrary.printText(3, "Hallo", 3);
+
+        Scanner sc = new Scanner(System.in);
+
+        UtilsLibrary.printText(1, "Hallo, bitte geben sie ein Password ein: ", 0);
+        String InputPassword = sc.nextLine();
+
+        UtilsLibrary.printText(1, "Ihr Passwort lautet: " + InputPassword, 1);
+
+        String outPut = "Passwort akzeptiert!";
+        if(!PasswordValidator.isValid(InputPassword)) {
+
+            outPut = "Passwort nicht akzeptiert!";
+        }
+
+        UtilsLibrary.printText(2, outPut, 1);
+
     }
 }
