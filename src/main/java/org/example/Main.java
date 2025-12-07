6181690a4f3b892af431eb0ec8d516b83c6ca387
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-07 22:01:12 +0100

    update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,66 +8,80 @@ import org.example.Utils.UtilsLibrary;
 import java.util.Scanner;
 
 public class Main {
-    static void main() {
+
+    static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         boolean bInput = false;
         boolean bGenerate = false;
 
-        while (!bInput || !bGenerate) {
+        // until one is true
+        while (!bInput && !bGenerate) {
 
             UtilsLibrary.printText(1, "Hallo, wie kann ich ihnen helfen?", 0);
             UtilsLibrary.printText(0, "1 = Passwort eingeben, 2 = Passwort automatisch generieren, 0 = Exit", 0);
-            String Input = sc.nextLine();
-            if (Input.isEmpty() || !Input.matches("[0-2]")) {
 
+            String input = sc.nextLine();
+            if (input.isBlank() || !Character.isDigit(input.charAt(0))) {
                 continue;
             }
 
-            if (Input.matches("1")) {
-                bInput = true;
-            } else if (Input.matches("2")) {
-                bGenerate = true;
-            } else {
-                UtilsLibrary.printText(1, "Auf Wiedersehen!", 1);
-                return;
+            int num = Integer.parseInt(input);
+            switch (num) {
+                case 1:
+                    bInput = true;
+                    break;
+                case 2:
+                    bGenerate = true;
+                    break;
+                case 0:
+                    UtilsLibrary.printText(1, "Auf Wiedersehen!", 1);
+                    return;
+                default:
+                    continue;
+
             }
         }
 
+        // TODO: list input allowed
         UtilsLibrary.printText(1, "Bitte geben sie ihren Namen ein: ", 0);
         String Name = sc.nextLine();
-        UtilsLibrary.printText(1, "Hallo" + Name, 1);
-
-        String password = "a";
-        String confirmationPassword = "b";
+        UtilsLibrary.printText(1, "Hallo " + Name, 1);
 
         // TODO:add while password != confirmationPassword or 0 = exit
         if (bInput) {
 
-            while (!PasswordValidator.isValid(password)) {
+            boolean bContinue = true;
+
+            while (bContinue) {
 
                 UtilsLibrary.printText(0, "Bitte Passwort eingeben: ", 0);
-                password = sc.nextLine();
+
+                String password = sc.nextLine();
+                if (PasswordValidator.isValid(password))
+                    bContinue = false;
+                ;
             }
 
-            while (!password.equals(confirmationPassword)) {
 
-                UtilsLibrary.printText(1, "Bitte Passwort bestätigen: ", 0);
-                confirmationPassword = sc.nextLine();
-
-                if (confirmationPassword.equals(password)) {
-                    UtilsLibrary.printText(1, "Danke " + Name, 0);
-                    UtilsLibrary.printText(0, "Ihr Passwort wurde gespeichert", 0);
-
-                    String savedPassword = confirmationPassword;
-
-                    UtilsLibrary.printText(1, "Auf wiedersehen...", 1);
-
-                } else {
-                    UtilsLibrary.printText(1, "Eingaben stimmen nicht überein!", 1);
-                }
-            }
+//            while (!password.equals(confirmationPassword)) {
+//
+//                UtilsLibrary.printText(1, "Bitte Passwort bestätigen: ", 0);
+//                confirmationPassword = sc.nextLine();
+//
+//                if (confirmationPassword.equals(password)) {
+//                    UtilsLibrary.printText(1, "Danke " + Name, 0);
+//                    UtilsLibrary.printText(0, "Ihr Passwort wurde gespeichert", 0);
+//
+//                    String savedPassword = confirmationPassword;
+//
+//                    UtilsLibrary.printText(1, "Auf wiedersehen...", 1);
+//
+//                } else {
+//                    UtilsLibrary.printText(1, "Eingaben stimmen nicht überein!", 1);
+//                }
+//            }
 
         }
     }
